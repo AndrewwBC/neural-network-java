5617609c1228ac8924992890ce5e953354074a7b
AndrewwBC <andrew-hot2011@hotmail.com>
2025-02-16 22:22:54 -0300

    add error method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Neuron neuron = new Neuron(1.2,1.2,10.0);
+        System.out.println(neuron.error());
     }
 }
--- src/main/java/org/example/Neuron.java
+++ src/main/java/org/example/Neuron.java
@@ -7,15 +7,31 @@ public class Neuron {
     private Double bias;
     private Double input;
 
+    private static final double LEARNING_RATE = 0.1;
+
     public Neuron(Double weight, Double bias, Double input){
         this.weigth = weight;
         this.bias = bias;
         this.input = input;
     }
 
-    public Double sum(){
+    public Double predict(){
         return (weigth * input) + bias;
     }
 
+
+    public String error(){
+        var x = 2;
+
+        var YR = x + 1;
+
+        var errorInFunctionOfWeight = -2*( ( YR - this.predict()) ) * x;
+
+        var errorInFunctionOfBias = -2*( (YR - this.predict()) ) ;
+
+        System.out.println(errorInFunctionOfWeight);
+        System.out.println(errorInFunctionOfBias);
+        return "Oi";
+    }
 
 }
